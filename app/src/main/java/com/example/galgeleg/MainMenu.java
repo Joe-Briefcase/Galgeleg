d1cc6ebabc5f29c35ef55ad65d27e9b768a46371
Joe-Briefcase <s185143@student.dtu.dk>
2019-11-07 15:13:28 +0100

    Begyndt på ny opgave.

--- app/src/main/java/com/example/galgeleg/MainMenu.java
+++ app/src/main/java/com/example/galgeleg/MainMenu.java
@@ -1,10 +1,13 @@
 package com.example.galgeleg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
